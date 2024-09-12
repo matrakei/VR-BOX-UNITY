58314ec49bbbf61522a5bfc8aa44d334c99cd6c7
matrakei <163850982+matrakei@users.noreply.github.com>
2024-09-12 14:59:08 -0300

    FUNCA

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -8,7 +8,9 @@ public class SoundManager : MonoBehaviour
     public static SoundManager Instance;
     public AudioSource Audios;
     public AudioSource SFX;
-    public AudioClip BasicPunch;
+    public AudioSource OTHERSFX;
+    public AudioClip[] Kicks;
+    int random;
 
     // Start is called before the first frame update
     void Awake()
@@ -25,18 +27,23 @@ public class SoundManager : MonoBehaviour
     }
 
 
-    private void PlayMusic(AudioClip clip)
+    public void PlayMusic(AudioClip clip)
     {
         Audios.clip = clip;
         Audios.Play();
     }
 
-    private void PlaySound(AudioClip clip)
+    public void PlaySound(AudioClip clip)
     {
         SFX.PlayOneShot(clip);
     }
+    public void PlayOtherSFX(AudioClip clip)
+    {
+        OTHERSFX.PlayOneShot(clip);
+    }
     public void BasicPunchSFX()
     {
-        SFX.PlayOneShot(BasicPunch);
+        random = Random.Range(0, Kicks.Length);
+        SFX.PlayOneShot(Kicks[random]);
     }
 }
